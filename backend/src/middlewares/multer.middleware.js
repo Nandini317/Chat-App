5d4e34d53e653d81e26de2208e8b5f142ace73c9
Nandini <154619218+Nandini317@users.noreply.github.com>
2026-01-01 23:53:00 +0530

    Fix indentation in multer middleware filename function

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -5,8 +5,8 @@ const storage = multer.diskStorage({
       cb(null, './public/temp')
     },
     filename: function (req, file, cb) {
-    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) //baad mein krenge 
-      cb(null, file.originalname) // file.fieldname + '-' + uniqueSuffix : we can use this later 
+    
+      cb(null, file.originalname) 
     }
   })
   
